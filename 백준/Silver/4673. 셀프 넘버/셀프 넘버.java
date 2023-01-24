38aa215688c1b860bf0e69b3ad503bea75cb2c4b
ryunhyeong <94901867+ryunhyeong@users.noreply.github.com>
2023-01-24 22:05:34 +0900

    [Silver V] Title: 셀프 넘버, Time: 152 ms, Memory: 14616 KB -BaekjoonHub

---
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
@@ -0,0 +1,41 @@
+public class Main{
+
+	static int count[] = new int[10000];
+	public static void sum(int a) {
+		count[a]++;
+		if(a>10000 || count[a]>=2) {
+			return;
+		}
+	    int ans = 0;
+	    ans += a;
+        
+	    while(true) {
+	    	if(a<10) {
+	    		ans += a % 10;
+	    		break;
+	    	}
+	    	else if(a >= 10) {
+	    		ans += a % 10;
+	    		a = a / 10;
+	    	}
+	    }
+	    if(ans>=10000) {
+	    	return;
+	    }
+    	sum(ans);
+    
+	}
+	
+    public static void main(String args[]){
+        for(int i =1;i<=9999;i++) {
+        	sum(i);
+        }
+        int j = 0;
+        for(int k:count) {
+        	if(k<2 && j!=0) {
+        		System.out.println(j);
+        	}
+        	j++;
+        }
+    }
+}
