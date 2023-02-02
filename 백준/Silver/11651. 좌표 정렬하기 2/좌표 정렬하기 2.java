b86735543cf9edfa65980e48e928bcd305f3fcb8
ryunhyeong <94901867+ryunhyeong@users.noreply.github.com>
2023-02-02 23:17:50 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 1468 ms, Memory: 58812 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,53 @@
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.math.BigInteger;
+import java.security.DrbgParameters;
+import java.awt.print.Paper;
+import java.awt.print.Printable;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.util.function.IntPredicate;
+
+import javax.naming.spi.DirStateFactory.Result;
+
+public class Main{
+    public static void main(String args[]) throws IOException{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        //int N = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(bf.readLine());
+        StringTokenizer st;
+        int arr[][] = new int[N][2];
+        for(int i =0; i<N; i++) {
+        	st = new StringTokenizer(bf.readLine());
+        	arr[i][0] = Integer.parseInt(st.nextToken());
+        	arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr,(o1, o2) ->{
+			if(o1[1]==o2[1]) {
+				return o1[0] - o2[0];
+			}
+			else {
+				return o1[1] - o2[1];
+			}			
+        });
+        for(int i=0; i<N; i++) {
+        	System.out.println(arr[i][0] + " " + arr[i][1]);
+        }
+    }
+}
+    
+        		
+ 
+        				
+   
+
+
+
+        				
+        				
